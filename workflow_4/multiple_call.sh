a52daa2aef022155a8dc3a534b4ab85d2a05cbf3
Olivier <olivier.boulle@inria.fr>
2021-04-22 07:50:24 +0000

    added run command

--- workflow_4/multiple_call.sh
+++ workflow_4/multiple_call.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+#nohup ./multiple_call.sh &
 
 SCRIPT_PATH="workflow_4.sh"
-
+SCRIPT_PATH_2="workflow_4_C.sh"
 n_iter=10
 
 for (( i=0; i <= $n_iter; i++ ));
 do 
 	echo "_________run $i _________";
-	. "$SCRIPT_PATH"; 
+	. "$SCRIPT_PATH";
+	. "$SCRIPT_PATH_2";
 done
 
 
