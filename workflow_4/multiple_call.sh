cc7ec997c027c652e4e59b0dc9ba4d918cded2f6
oboulle <olivier.boulle@inria.fr>
2021-05-19 15:13:30 +0200

    changes

--- workflow_4/multiple_call.sh
+++ workflow_4/multiple_call.sh
@@ -3,14 +3,23 @@
 #nohup ./multiple_call.sh &
 
 SCRIPT_PATH="workflow_4.sh"
-SCRIPT_PATH_2="workflow_4_C.sh"
-n_iter=10
+n_iter=2
 
 for (( i=0; i <= $n_iter; i++ ));
 do 
 	echo "_________run $i _________";
-	. "$SCRIPT_PATH";
-	. "$SCRIPT_PATH_2";
+	. "$SCRIPT_PATH" -n 125
+	. "$SCRIPT_PATH" -n 150
+	. "$SCRIPT_PATH" -n 175
+	. "$SCRIPT_PATH" -n 200
+	. "$SCRIPT_PATH" -n 225
+	. "$SCRIPT_PATH" -n 250
+	. "$SCRIPT_PATH" -n 275
+	. "$SCRIPT_PATH" -n 300
+	. "$SCRIPT_PATH" -n 325
+	. "$SCRIPT_PATH" -n 350
+	. "$SCRIPT_PATH" -n 375
+	. "$SCRIPT_PATH" -n 400
 done
 
 
