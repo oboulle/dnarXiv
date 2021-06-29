471a8e55fa6f8c4119c00ecd3ddb5cd1bae8cc95
oboulle <olivier.boulle@inria.fr>
2021-06-29 11:52:49 +0200

    initial commit

---
+++ commands/dna_del.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#-----------------------------------------------#
+######### ====== read parameters ====== #########
+#-----------------------------------------------#
+
+help_function() {
+   echo ""
+   echo "Usage: dna_del Cname DI"
+   exit 1 # Exit script after printing help
+}
+
+container_path="${1}"
+document_index="${2}"
+
+if [ "$#" != 2 ]
+then
+	echo "error invalid number of arguments"
+	help_function
+	exit 1
+fi
+
+if [ ! -d "$container_path" ] 
+then
+    echo "error : container $container_path not found" 
+    exit 1
+fi
+
+if [ ! -d "$container_path/$document_index" ] 
+then
+    echo "error : $container_path does not contain a document of index $document_index" 
+    exit 1
+fi
+
+#-----------------------------------------------#
+######### ====== delete document ====== #########
+#-----------------------------------------------#
+
+cdi_file="$container_path/.cdi"
+current_document_index=$(head -n 1 "$cdi_file")
+
+if (( $current_document_index < 0 ))
+then
+	echo "the container is not editable"
+	exit 1
+fi
+
+rm -rf "$container_path/$document_index"
+echo "Document $document_index successfully deleted from $container_path !"
+
+exit 0
