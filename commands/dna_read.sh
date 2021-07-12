0f3c32a283984404178913e0ae83511c950f5164
BOULLE Olivier <olivier.boulle@inria.fr>
2021-07-12 16:13:46 +0200

    initial commit - not finished

---
+++ commands/dna_read.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#----- default parameters -----#
+n_synth=100
+i_error=0
+d_error=0
+s_error=0
+
+#-----------------------------------------------#
+######### ====== read parameters ====== #########
+#-----------------------------------------------#
+
+help_function() {
+   echo ""
+   echo "Usage: dna_read Cname DI Dname"
+   exit 1 # Exit script after printing help
+}
+
+container_path="${1}"
+document_index="${2}"
+document_path="${3}"
+
+if [ "$#" != 3 ]
+then
+	echo "error invalid number of arguments"
+	help_function
+	exit 1
+fi
+
+if [ ! -d "$container_path" ] 
+then
+    echo "error : container $container_path not found" 
+    exit 1
+fi
+
+if [ ! -d "$container_path/$document_index" ] 
+then
+    echo "error : document $document_index of $container_path not found" 
+    exit 1
+fi
+
+#---------------------------------------------#
+######### ====== read document ====== #########
+#---------------------------------------------#
+
+cdi_file="$container_path/.cdi"
+container_index=$(head -n 1 "$cdi_file")
+
+if (( $container_index > 0 ))
+then
+	echo "the container is not readable"
+	exit 1
+fi
+
+working_dir=$(pwd)
+
+if [ $HOME == "/Users/oboulle" ]
+then
+  project_dir="/Users/oboulle/Documents"
+else
+  project_dir="/home/oboulle/Documents"
+fi
+
+#get metadata informations of META
+#if META not already sequenced
+#sequence META dir
+
+#get metadata informations of DI
+
+#sequence DI
+
+echo "Document $document_index of $container_path successfully saved to $document_path !"
+exit 0
