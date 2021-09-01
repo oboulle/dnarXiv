9ba73bc687e1766086a5bf28c6b4c4ba3f8d0e28
BOULLE Olivier <olivier.boulle@inria.fr>
2021-09-01 11:47:51 +0200

    scp command, rm old workflow

--- dnarXiv_commands/dna_workflow.sh
+++ dnarXiv_commands/dna_workflow.sh
@@ -8,6 +8,7 @@ else
 fi
 
 commands_dir="$project_dir/workflow_global/dnarXiv_commands"
+rm -rf test_workflow_old
 mv test_workflow test_workflow_old
 $commands_dir/dna_create.sh -sim -fl 100 test_workflow 
 $commands_dir/dna_add.sh $commands_dir/doc.txt test_workflow
@@ -17,3 +18,5 @@ $commands_dir/dna_store.sh test_workflow
 $commands_dir/dna_read.sh test_workflow 0 test_workflow/resultat_du_workflow.txt
 
 exit 0
+
+# scp oboulle@dnarxiv.irisa.fr:~/Documents/workflow_global/dnarXiv_commands/resultat_du_workflow.png .
