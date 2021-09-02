e91aaa19e0612d5cff727c065b763dff7e625091
BOULLE Olivier <olivier.boulle@inria.fr>
2021-09-02 09:44:12 +0200

    fix error in times location

--- dnarXiv_commands/dna_create.sh
+++ dnarXiv_commands/dna_create.sh
@@ -53,7 +53,7 @@ echo "sim=$simulation frag_length=$frag_length spacer=$spacer container=$contain
 #----------------------------------------------------------#
 ######### ====== create directory and files ====== #########
 #----------------------------------------------------------#
-echo workflow $(date +"%Hh%Mm%S") >> "$container_path/workflow_times.txt"
+echo workflow $(date +"%Hh%Mm%S") >> "$container_name/workflow_times.txt"
 time=$(date +"%s")
 
 container_path="$(pwd)/$container_name"
