032d09546b5c1d3b99f7ac77b4f41264ddf25a53
oboulle <olivier.boulle@inria.fr>
2021-09-20 15:17:08 +0200

    init

--- result_analysis/dna_performances_tests.sh
+++ result_analysis/dna_performances_tests.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-SCRIPT_PATH="../dna_workflow.sh"
-n_mol_list=`seq 40 -5 0` #'seq first step last'; decrement because lowest n_mol will be more likely to fail
+
+n_mol_list=`seq 5 5 40` #'seq first step last'
 same_script_count=5 #number of time the exact same script is executed
+container_name=test_perf_container
+document_name=doc.txt
+
+(exec ../dna_workflow.sh -no_read $container_name $document_name) #init a container 
 
 for n_mol in $n_mol_list; do
 	for i in $(seq $same_script_count); do
 		echo ______________ n_mol $n_mol : $i ______________
-	    (exec "$SCRIPT_PATH" $n_mol)
+		rm -rf $container_name/0/{6*,7*,8*,9*,10*,11*,12*,workflow_times.txt} #remove previous reading files
+	    (exec ../dna_read.sh -n_mol $n_mol $container_name 0 $container_name/0/12_result_$document_name)
+	    python3 save_results.py $container_name/0 results_workflow.txt
 	done
 done
 
