ba6601b1d889001b338e1baf4213cba48d0a90bc
oboulle <olivier.boulle@inria.fr>
2021-09-20 14:11:03 +0200

    improved saving results

--- dna_workflow.sh
+++ dna_workflow.sh
@@ -6,6 +6,7 @@ check_error_function () { #end the program if the previously called script has r
 	if [ ! $? = 0 ]
 	then
 		echo "error in $1"
+		save_results_function
 		exit 1
 	fi
 }
@@ -22,12 +23,18 @@ fi
 
 commands_dir="$project_dir"/workflow_commands
 container_name="test_workflow"
-document_name="img_50.png"
+document_name="doc.txt" #"img_50.png"
+
+save_results_function () {
+	save_results_script="$commands_dir"/result_analysis/save_results.py
+	results_file="$commands_dir"/results_workflow.txt
+	python3 "$save_results_script" "$container_name"/0 "$results_file"
+}
 
 rm -rf "$container_name"_old
 mv "$container_name" "$container_name"_old #save the previous workflow
 
-"$commands_dir"/dna_create.sh -sim -fl 100 "$container_name"
+"$commands_dir"/dna_create.sh -sim "$container_name"
 check_error_function "dna_create"
 
 "$commands_dir"/dna_add.sh "$commands_dir"/"$document_name" "$container_name"
@@ -36,18 +43,17 @@ check_error_function "dna_add"
 "$commands_dir"/dna_store.sh "$container_name"
 check_error_function "dna_store"
 
-"$commands_dir"/dna_read.sh "$container_name" 0 "$container_name"/result_"$document_name"
+if test "$1" #number of molecules to read is defined
+then
+	"$commands_dir"/dna_read.sh -n_mol $1 "$container_name" 0 "$container_name"/result_"$document_name"
+else
+	"$commands_dir"/dna_read.sh "$container_name" 0 "$container_name"/result_"$document_name"
+fi
+
 check_error_function "dna_read"
 
+save_results_function
 
-#----Result Analysis----#
-
-save_results_script="$commands_dir"/result_analysis/save_results.py
-results_file="$commands_dir"/results_workflow.txt
-python3 "$save_results_script" "$container_name"/0 "$results_file"
-
-
-exit 0
 
 #command to copy the total workflow from the dnarxiv server
 # scp -r oboulle@dnarxiv.irisa.fr:~/Documents/workflow_commands/test_workflow .
