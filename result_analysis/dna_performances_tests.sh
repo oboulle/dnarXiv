046dc84586662225fb639f42a519b093d409be70
oboulle <olivier.boulle@inria.fr>
2021-09-21 14:24:22 +0200

    save the result doc

--- result_analysis/dna_performances_tests.sh
+++ result_analysis/dna_performances_tests.sh
@@ -2,7 +2,7 @@
 
 
 n_mol_list=`seq 5 5 40` #'seq first step last'
-same_script_count=5 #number of time the exact same script is executed
+same_script_count=5 #number of time the script is executed with the same parameters
 container_name=test_perf_container
 document_name=doc.txt
 
@@ -12,7 +12,7 @@ for n_mol in $n_mol_list; do
 	for i in $(seq $same_script_count); do
 		echo ______________ n_mol $n_mol : $i ______________
 		rm -rf $container_name/0/{6*,7*,8*,9*,10*,11*,12*,workflow_times.txt} #remove previous reading files
-	    (exec ../dna_read.sh -n_mol $n_mol $container_name 0 $container_name/0/12_result_$document_name)
+	    (exec ../dna_read.sh -n_mol $n_mol $container_name 0 results/${n_mol}_${i}_$document_name)
 	    python3 save_results.py $container_name/0 results_workflow.txt
 	done
 done
