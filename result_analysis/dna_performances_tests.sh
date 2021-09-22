dbd4f827c482ede4ddc575b83df8558fa139d03f
oboulle <olivier.boulle@inria.fr>
2021-09-22 10:47:53 +0200

    removed same script count

--- result_analysis/dna_performances_tests.sh
+++ result_analysis/dna_performances_tests.sh
@@ -2,19 +2,17 @@
 
 
 n_mol_list=`seq 5 5 40` #'seq first step last'
-same_script_count=3 #number of time the script is executed with the same parameters
 container_name=test_perf_container
 document_name=doc.txt
 
 (exec ../dna_workflow.sh -no_read $document_name $container_name) #init a container 
 
 for n_mol in $n_mol_list; do
-	for i in $(seq $same_script_count); do
-		echo ______________ n_mol $n_mol : $i ______________
-		rm -rf $container_name/0/{6*,7*,8*,9*,10*,11*,12*,workflow_times.txt} #remove previous reading files
-	    (exec ../dna_read.sh -n_mol $n_mol $container_name 0 results/${n_mol}_${i}_$document_name)
-	    python3 save_results.py $container_name/0 results_workflow.txt
-	done
+	echo ______________ n_mol $n_mol : $i ______________
+	rm -rf $container_name/0/{6*,7*,8*,9*,10*,11*,12*} #remove previous reading files
+	mv $container_name/0/workflow_times.txt $container_name/workflow_times.txt
+	(exec ../dna_read.sh -n_mol $n_mol $container_name 0 results/${n_mol}_${i}_$document_name)
+	python3 save_results.py $container_name/0 results_workflow.txt
 done
 
 #-------------- Exit --------------#
