03a8ca70dae310b00766947c1352f64aa06a6ea6
oboulle <olivier.boulle@inria.fr>
2021-09-22 10:04:02 +0200

    removed repeat number

--- result_analysis/plot_results.py
+++ result_analysis/plot_results.py
@@ -3,18 +3,8 @@ import os
 import sys
 import matplotlib.pyplot as plt
 
-def compute_array(array, repeat_nbr):
-    computed_array = []
-    
-    for i in range(int(len(array)/repeat_nbr)):
-        sub_array = array[repeat_nbr*i:repeat_nbr*(i+1)]
-        while None in sub_array: sub_array.remove(None)
-        if sub_array == []: computed_array.append(None)
-        else: computed_array.append(sum(sub_array) / len(sub_array))
-    return computed_array
 
-
-def compute_results(results_path, repeat_nbr):
+def compute_results(results_path):
     
     if not os.path.isfile(results_path):
         print("error : file not found : "+results_path)
@@ -32,32 +22,26 @@ def compute_results(results_path, repeat_nbr):
         n_mol = results_file.readline().replace("n_mol ", "").replace("\n","")
         precision = results_file.readline().replace("precision ", "").replace("\n","")
         reading_time = results_file.readline().replace("reading_time ", "").replace("\n","")
-        
-        if source_length != "None":
+        line = results_file.readline()
+        line = results_file.readline()
+            
+        if source_length != "None" and n_mol != "None":
             source_length_array.append(int(source_length))
-        else:
-            source_length_array.append(None)
-        if n_mol != "None":
             n_mol_array.append(int(n_mol))
         else:
-            n_mol_array.append(None)
+            continue #skip the line
+        
         if precision != "None":
             precision_array.append(float(precision))
         else: #precision = None -> no result has been found -> 0% precision
             precision_array.append(0)
+        
         if reading_time != "None":
             reading_time_array.append(int(reading_time))
         else:
-            reading_time_array.append(None)
-        
-        line = results_file.readline()
-        line = results_file.readline()
-    
-    computed_source_length_array = compute_array(source_length_array, repeat_nbr)
-    computed_n_mol_array = compute_array(n_mol_array,repeat_nbr)
-    computed_precision_array = compute_array(precision_array, repeat_nbr)
-    computed_reading_time_array = compute_array(reading_time_array, repeat_nbr)
-    return computed_source_length_array, computed_n_mol_array, computed_precision_array, computed_reading_time_array
+            reading_time_array.append(0)
+
+    return source_length_array, n_mol_array, precision_array, reading_time_array
 
 
 def plot_arrays(array_x, array_y, label_x, label_y):
@@ -66,6 +50,8 @@ def plot_arrays(array_x, array_y, label_x, label_y):
     
     plt.xlabel(label_x)
     plt.ylabel(label_y)
+
+    if label_y == "precision": plt.ylim(0,100)
     plt.show()
 
 # =================== main ======================= #
@@ -74,13 +60,11 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("usage : plot_results.py results_path")
         sys.exit(1)
-
+    
     results_path = sys.argv[1]
-    repeat_nbr = 3 #number of time the scripts have been repeated with the same parameters
-    source_length, n_mol, precision, reading_time = compute_results(results_path, repeat_nbr)
+    source_length, n_mol, precision, reading_time = compute_results(results_path)
     
     plot_arrays(n_mol, reading_time, "n_mol", "reading_time (s)")
     plot_arrays(n_mol, precision, "n_mol", "precision")
-
     
     print("\tcompleted !")
