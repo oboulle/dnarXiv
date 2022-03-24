1c6fae8d32997c0e9f8587bf4959bf9feac2e523
oboulle <olivier.boulle@inria.fr>
2022-03-24 11:59:07 +0100

    updated test and added error check

--- dna_quick_complete_test.sh
+++ dna_quick_complete_test.sh
@@ -10,13 +10,34 @@ else
 	project_dir="/home/oboulle/Documents"
 fi
 
+check_error_function () { 
+	if [ ! $? = 0 ]
+	then
+		echo "error in $1"
+		echo "cancel dna_add"
+		rm -rf $stored_document_path
+		exit 1
+	fi
+}
+
 commands_dir="$project_dir"/workflow_commands
 container_name="test_workflow"
 rm -rf "$container_name"
+
 "$commands_dir"/dna_create.sh -sim -fl 100 "$container_name" 
-"$commands_dir"/dna_add.sh "$commands_dir"/doc.txt "$container_name"
-"$commands_dir"/dna_store.sh "$container_name" 
+check_error_function "dna_create"
+
+"$commands_dir"/dna_add.sh -nocd "$commands_dir"/documents_test/doc.txt "$container_name"
+check_error_function "dna_add"
+
+"$commands_dir"/dna_store.sh "$container_name"
+check_error_function "dna_store"
+
 "$commands_dir"/dna_list.sh "$container_name"
+check_error_function "dna_list"
+
 "$commands_dir"/dna_read.sh "$container_name" 0 resultat_du_workflow.txt
+check_error_function "dna_read"
+
 
 exit 0
