0c6058e2debcf530c9aec3c90d645f771f25ad0d
oboulle <103439053+oboulle@users.noreply.github.com>
2022-04-13 17:23:39 +0200

    installation of julia

--- install.sh
+++ install.sh
@@ -4,3 +4,12 @@ git submodule init && git submodule update #clone the git subprojects
 
 sudo apt-get update
 sudo apt-get install xmlstarlet
+
+#install julia
+if [ ! -f /usr/local/bin/julia ]; then
+	wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.2-linux-x86_64.tar.gz
+	tar -xvzf julia-1.7.2-linux-x86_64.tar.gz
+	sudo cp -r julia-1.7.2 /opt/
+	sudo ln -s /opt/julia-1.7.2/bin/julia /usr/local/bin/julia
+	rm -rf julia-*
+fi
