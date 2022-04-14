765dd24bd727cc945d1322952ca5b0aafbded756
oboulle <olivier.boulle@inria.fr>
2022-04-14 17:09:51 +0200

    let user install xmlstarlet

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # dnarXiv
 
+# Install
+## Prerequisites
+
+install xmlstarlet : http://xmlstar.sourceforge.net/download.php
+(XMLStarlet is a set of command line utilities (toolkit) to query, transform, validate, and edit XML documents and files using a simple set of shell commands)
+
+## Get the workflow
+
 git clone https://github.com/oboulle/dnarXiv
 
 cd dnarXiv
--- install.sh
+++ install.sh
@@ -2,10 +2,7 @@
 
 git submodule init && git submodule update #clone the git subprojects
 
-sudo apt-get update
-sudo apt-get install xmlstarlet
-
-#install julia
+#install julia if not in the machine
 if [ ! -f /usr/local/bin/julia ]; then
 	wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.2-linux-x86_64.tar.gz
 	tar -xvzf julia-1.7.2-linux-x86_64.tar.gz
